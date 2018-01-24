1618b57ff6fddb42107b560dcdcd4da9ed2c7a61
Andrew Bares <anbare@microsoft.com>
2018-01-23 20:54:37 -0800

    Forgot one file

--- Win32Extensions/WpfDesktopToastsApp/MyNotificationActivator.cs
+++ Win32Extensions/WpfDesktopToastsApp/MyNotificationActivator.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
+﻿using Microsoft.QueryStringDotNET;
+using Microsoft.Toolkit.Uwp.Notifications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,47 +13,128 @@ using Windows.UI.Notifications;
 
 namespace WpfDesktopToastsApp
 {
+    // The GUID must be unique to your app. Create a new GUID if copying this code.
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComSourceInterfaces(typeof(INotificationActivationCallback))]
+    [Guid("7956E95C-D42E-413B-9C8E-C173E6ADF0C3"), ComVisible(true)]
     public class MyNotificationActivator : NotificationActivator
     {
         public override void OnActivated(string invokedArgs, NotificationUserInput userInput, string appUserModelId)
         {
-            MessageBox.Show(invokedArgs);
+            Application.Current.Dispatcher.Invoke(delegate
+            {
+                // Parse the query string (using QueryString.NET)
+                QueryString args = QueryString.Parse(invokedArgs);
 
-            //// Construct the visuals of the toast
-            //ToastContent toastContent = new ToastContent()
-            //{
-            //    // Arguments when the user taps body of toast
-            //    Launch = "action=ok",
+                // See what action is being requested 
+                switch (args["action"])
+                {
+                    // Open the image
+                    case "viewImage":
 
-            //    Visual = new ToastVisual()
-            //    {
-            //        BindingGeneric = new ToastBindingGeneric()
-            //        {
-            //            Children =
-            //            {
-            //                new AdaptiveText()
-            //                {
-            //                    Text = "Toast was clicked"
-            //                },
+                        // The URL retrieved from the toast args
+                        string imageUrl = args["imageUrl"];
 
-            //                new AdaptiveText()
-            //                {
-            //                    Text = invokedArgs
-            //                }
-            //            }
-            //        }
-            //    }
-            //};
+                        // Make sure we have a window open (in case user clicked this while app closed)
+                        if (App.Current.Windows.Count == 0)
+                        {
+                            new MainWindow().Show();
+                        }
 
-            //var doc = new XmlDocument();
-            //doc.LoadXml(toastContent.GetContent());
+                        // And then show the image
+                        (App.Current.Windows[0] as MainWindow).ShowImage(imageUrl);
 
-            //// And create the toast notification
-            //var toast = new ToastNotification(doc);
+                        break;
 
-            //// And then show it
-            //// If non-Desktop Bridge app, you must provide your AUMID
-            //ToastNotificationManager.CreateToastNotifier(App.AUMID).Show(toast);
+
+                    // Open the conversation
+                    case "viewConversation":
+
+                        // The conversation ID retrieved from the toast args
+                        int conversationId = int.Parse(args["conversationId"]);
+
+
+                        // Make sure we have a window open (in case user clicked this while app closed)
+                        if (App.Current.Windows.Count == 0)
+                        {
+                            new MainWindow().Show();
+                        }
+
+                        // And then show the conversation
+                        (App.Current.Windows[0] as MainWindow).ShowConversation();
+                        break;
+
+                    // Background: Quick reply to the conversation
+                    case "reply":
+
+                        // Get the response the user typed
+                        string msg = userInput["tbReply"];
+
+                        // And send this message (we'll just show a toast)
+                        ShowToast("Sending message: " + msg);
+
+                        // If there's no windows open, exit the app
+                        if (App.Current.Windows.Count == 0)
+                        {
+                            Application.Current.Shutdown();
+                        }
+
+                        break;
+
+                    // Background: Send a like
+                    case "like":
+
+                        // Send a like (we'll just show a toast)
+                        ShowToast("Sent a like!");
+
+                        // If there's no windows open, exit the app
+                        if (App.Current.Windows.Count == 0)
+                        {
+                            Application.Current.Shutdown();
+                        }
+
+                        break;
+
+                    default:
+
+                        new MainWindow().Show();
+                        break;
+                }
+            });
+        }
+
+        private void ShowToast(string msg)
+        {
+            // Construct the visuals of the toast
+            ToastContent toastContent = new ToastContent()
+            {
+                // Arguments when the user taps body of toast
+                Launch = "action=ok",
+
+                Visual = new ToastVisual()
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        Children =
+                        {
+                            new AdaptiveText()
+                            {
+                                Text = msg
+                            }
+                        }
+                    }
+                }
+            };
+
+            var doc = new XmlDocument();
+            doc.LoadXml(toastContent.GetContent());
+
+            // And create the toast notification
+            var toast = new ToastNotification(doc);
+
+            // And then show it
+            // If non-Desktop Bridge app, you must provide your AUMID
+            ToastNotificationManager.CreateToastNotifier(App.AUMID).Show(toast);
         }
     }
 }
